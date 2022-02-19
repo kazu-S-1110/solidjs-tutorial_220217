fff18f5c301bee62d994ba6da8a0fe3e2fd16a9d
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-19 19:55:02 +0900

    派生signalについて学習。おお、Reactを違う！

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,14 +2,16 @@ import { createSignal, createEffect } from 'solid-js';
 
 export const Counter = () => {
   const [count, setCount] = createSignal<number>(0);
-  // setInterval(() => setCount(count() + 1), 1000);
+  setInterval(() => setCount(count() + 1), 1000);
+  const doubleCount = () => count() * 2;
+
   createEffect(() => {
-    console.log(`The count is now ${count()}`);
+    console.log(`The count is now ${doubleCount()}`);
   });
 
   return (
     <>
-      <div>Count : {count()}</div>
+      <div>Count : {doubleCount()}</div>
       <button onclick={() => setCount(count() + 1)}>plus me</button>
     </>
   );
