14c02f2395b16aaffb1e9ddc3885ae0c1674b5d3
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-19 19:51:18 +0900

    createEffectについて学習。useEffectの全対象版、、、？

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,16 @@
-import { createSignal } from 'solid-js';
+import { createSignal, createEffect } from 'solid-js';
 
 export const Counter = () => {
   const [count, setCount] = createSignal<number>(0);
-  setInterval(() => setCount(count() + 1), 1000);
+  // setInterval(() => setCount(count() + 1), 1000);
+  createEffect(() => {
+    console.log(`The count is now ${count()}`);
+  });
 
-  return <div>Count : {count()}</div>;
+  return (
+    <>
+      <div>Count : {count()}</div>
+      <button onclick={() => setCount(count() + 1)}>plus me</button>
+    </>
+  );
 };
