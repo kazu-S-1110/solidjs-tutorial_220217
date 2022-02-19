76439f36fbe73fa73b9b02f77497b02637b82b48
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-19 20:08:51 +0900

    createMemoについて学習、usememoだね。

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,18 +1,51 @@
-import { createSignal, createEffect } from 'solid-js';
+import { createSignal, createEffect, createMemo } from 'solid-js';
+
+const fibonacci: any = (num: number) => {
+  if (num <= 1) return 1;
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
 
 export const Counter = () => {
   const [count, setCount] = createSignal<number>(0);
-  setInterval(() => setCount(count() + 1), 1000);
-  const doubleCount = () => count() * 2;
+  // const timer = setInterval(() => {
+  //   setCount(count() + 1);
+  //   if (count() === 5) {
+  //     clearInterval(timer);
+  //   }
+  // }, 1000);
 
-  createEffect(() => {
-    console.log(`The count is now ${doubleCount()}`);
-  });
+  // const doubleCount = () => count() * 2;
+  const fib = createMemo(() => fibonacci(count()));
 
   return (
     <>
-      <div>Count : {doubleCount()}</div>
-      <button onclick={() => setCount(count() + 1)}>plus me</button>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+      <div>Count : {fib()}</div>
+
+      <button onclick={() => setCount(count() + 1)}>plus me {count()}</button>
     </>
   );
 };
