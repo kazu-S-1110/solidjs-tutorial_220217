bec431d91f11b48c43111c8f45b5ccd6d4fe336c
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-23 21:11:45 +0900

    通常のmapでやるんじゃなくてForを使って配列を展開するみたい。

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import Nested from './nested';
 import styles from './App.module.css';
 import { Counter } from './Counter';
 import { ShowComponent } from './ShowComponent';
+import { ForComponent } from './ForComponent';
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <h1>This is a Header</h1>
       <Nested />
-      <Counter />
-      <ShowComponent />
+      {/* <Counter /> */}
+      {/* <ShowComponent /> */}
+      <ForComponent />
     </div>
   );
 };
